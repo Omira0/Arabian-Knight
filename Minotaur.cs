9c75ffa92d64485c7c5a8ca9ac1e681ec20fd082
Abdelrahman Amira <152139312+Omira0@users.noreply.github.com>
2024-11-01 22:08:54 -0400

    Add files via upload
    Minotaur Scripts

---
+++ Minotaur.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minotaur : MonoBehaviour
+{
+    [SerializeField] float movement;
+    public bool isFacingRight = true;
+    [SerializeField] const float MinotaurSpeed = 2.5f;
+    public Rigidbody2D myRigidBody;
+    private void FixedUpdate()
+    {
+        myRigidBody.velocity = new Vector2(MinotaurSpeed * movement, myRigidBody.velocity.y);
+        if (movement < 0 && isFacingRight || movement > 0 && !isFacingRight)
+            Flip();
+    }
+    public void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        isFacingRight = !isFacingRight;
+    }
+
+
+
+
+
+
+
+}
